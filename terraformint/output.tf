ad930b195d2d4fbe2464df4b35007b77f7615ebd
ayam99 <58554115+ayam99@users.noreply.github.com>
2023-09-27 22:41:44 +0300

    Delete terraformint directory

--- terraformint/output.tf
+++
@@ -1,9 +0,0 @@
-output "instance_public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = aws_instance.web.public_ip
-}
-
-output "az" {
-  description = "az's in us-east-1"
-  value       = data.aws_availability_zones.available_azs
-}
